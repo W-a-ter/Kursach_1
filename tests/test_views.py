5f613a9fb994756c71401a8141ff58cea6325a02
verba4182 <verba4182@mail.ru>
2024-07-31 17:14:47 +0300

    kursach 31/07

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,9 @@
-from src.views import transactions
+from src.views import transactions, five_transactions
 
 
 def test_transactions(test_test):
+    assert test_test == []
+
+
+def test_five_transactions(test_test):
     assert test_test == []
